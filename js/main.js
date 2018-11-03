8c7b4095806031b0a1b37c31e76fd5a836d5ff22
xwen <wenberwen@outlook.com>
2018-11-02 22:24:22 -0300

    update project section design

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
         setTimeout(function(){
             $(".loading-wrapper").fadeOut(800);
         },1400);
+        $('.adigami').carousel({
+            interval: 3000
+          })
+          $('.gugi').carousel({
+            interval: 4500
+          })
     });
     function openNav() {
         $("#overlay").css ("height","100vh");
